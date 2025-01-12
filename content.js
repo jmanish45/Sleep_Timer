b2d98bd212399da5709a1902d2c0d20d2bf14010
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-01-12 17:02:40 +0000

    ok button removed

--- content.js
+++ content.js
@@ -1,12 +1,11 @@
-// content.js
 function stopPlayback() {
-    const videos = document.querySelectorAll('video');
-    videos.forEach(video => video.pause());
+  const videos = document.querySelectorAll('video');
+  videos.forEach(video => video.pause());
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "stopPlayback") {
+    stopPlayback();
+    sendResponse({ status: "Playback stopped" });
   }
-  
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "stopPlayback") {
-      stopPlayback();
-      sendResponse({ status: "Playback stopped" });
-    }
-  });
+});
